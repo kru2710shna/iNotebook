b46fba09f3c76911c7cc74c1c24a787dac54d923
Krushna Thakkar <109793947+kru2710shna@users.noreply.github.com>
2024-09-18 12:50:17 -0700

    Folder made: Models and Routes,for schema, routing from index.js

--- Backend/index.js
+++ Backend/index.js
@@ -5,9 +5,10 @@ const express = require('express')
 const app = express()
 const port = 3000
 
-app.get('/', (req, res) => {
-  res.send('Hello World!')
-})
+
+// Routes
+app.use('/api/auth' , require('./Routes/auth'))
+app.use('/api/notes' , require('./Routes/notes'))
 
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`)
