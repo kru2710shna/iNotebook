d979ee6855df27201ed9f614c7230512bf46ec90
Krushna Thakkar <109793947+kru2710shna@users.noreply.github.com>
2024-09-18 16:03:01 -0700

    added route under api/auth && connect database with backend vs code

--- Backend/Routes/auth.js
+++ Backend/Routes/auth.js
@@ -1,14 +1,26 @@
 const express = require('express');
 const User = require('../Models/User');
 const router = express.Router();
+const { body, validationResult } = require('express-validator');
 
 
 // create user using: POST '/api/auth'. Dosen't require auth
-router.post('/' , (req, res)=>{
-    console.log(req.body)
-    res.send(req.body)
-    const user = User(req.body);
-    user.save()
-}) 
+router.post('/', [
+    body('email', 'Enter valid Email').isEmail(),
+    body('name', 'Enter valid Name').isLength({ min: 3 }),
+    body('password', 'Enter valid Password').isLength({ min: 5 }),
+
+], (req, res) => {
+    const errors = validationResult(req)
+    if (!errors.isEmpty()) {
+        return res.status(400).json({ errors: errors.array() })
+    }
+    User.create({
+        name: req.body.name,
+        password: req.body.password,
+        email: req.body.email
+    }).then(user => res.json(user))
+        .catch(err => console.log(err))
+})
 
 module.exports = router
